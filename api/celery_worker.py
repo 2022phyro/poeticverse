d6609ffdedc0fca10280af075b5649d50eae1b69
2022phyro <phyrokelstein2@gmail.com>
2023-09-21 14:50:57 +0100

    Fixed all errors caused by rebase

--- api/celery_worker.py
+++
@@ -1,5 +0,0 @@
-from api import celery  # Import your Celery instance from api/__init__.py
-from utils.mailer import *  # Import your Celery tasks from utils/mailer.py
-from utils.notify import *
-if __name__ == '__main__':
-    celery.start()
--- run.sh
+++
@@ -1,4 +0,0 @@
-celery -A api.celery_worker worker --loglevel=info -P eventlet
-celery -A api.celery_worker beat
-flask --app "api:create_app()" run
-redis-server
